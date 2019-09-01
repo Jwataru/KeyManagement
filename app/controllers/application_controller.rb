a815d0ae7705d94846fb9d0ac54932b7ca43e298
Jwataru <jibiki@sorich.jp>
2019-09-01 20:21:43 +0900

    Username can't be blankになってしまう

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  # protect_from_forgery with: :exception
+
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # protected
+
+  #   def configure_permitted_parameters
+  #     # sign_inのときに、usernameも許可する
+  #     devise_parameter_sanitizer.for(:sign_in) << :user_name
+  #     # sign_upのときに、usernameも許可する
+  #     devise_parameter_sanitizer.for(:sign_up) << :user_name
+  #     #  account_updateのときに、usernameも許可する
+  #     devise_parameter_sanitizer.for(:account_update) << :user_name
+  #   end
 end
