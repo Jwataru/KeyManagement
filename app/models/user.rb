88a1e85a1f5942171a95b45f76a2b10940630189
Jwataru <jibiki@sorich.jp>
2019-09-02 18:04:03 +0900

    schemaとvalidateの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :username, presence: true, uniqueness: true
+  validates :user_name, presence: true, uniqueness: true
   has_many :keys
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_01_104843) do
+ActiveRecord::Schema.define(version: 2019_09_02_085809) do
 
   create_table "keys", force: :cascade do |t|
     t.integer "user_id"
@@ -29,12 +29,10 @@ ActiveRecord::Schema.define(version: 2019_09_01_104843) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username"
     t.string "user_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["user_name"], name: "index_users_on_user_name", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
