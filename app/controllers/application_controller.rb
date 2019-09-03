7c6960f14fdd1d72970049eb8804d9df7a108eb6
Jwataru <jibiki@sorich.jp>
2019-09-03 11:31:49 +0900

    StrongParameterの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,11 @@
 class ApplicationController < ActionController::Base
-  # protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
 
-  # protected
-
-  #   def configure_permitted_parameters
-  #     # sign_inのときに、usernameも許可する
-  #     devise_parameter_sanitizer.for(:sign_in) << :user_name
-  #     # sign_upのときに、usernameも許可する
-  #     devise_parameter_sanitizer.for(:sign_up) << :user_name
-  #     #  account_updateのときに、usernameも許可する
-  #     devise_parameter_sanitizer.for(:account_update) << :user_name
-  #   end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:user_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:user_name])
+  end
 end
